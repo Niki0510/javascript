98d5b2c2b465ee0a87d5dbdd946e43db3b16dbc8
Nikita Kupawdekar <77565967+Niki0510@users.noreply.github.com>
2023-08-26 22:32:48 +0530

    closure - return function

---
+++ day1.js
@@ -0,0 +1,8 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+function hello(){
+    return "Hello World!!";
+}
+
+let hWord = hello();
+console.log(hWord);
